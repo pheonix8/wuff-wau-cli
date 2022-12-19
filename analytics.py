8266e4dfc902e6a443db7f0b001fcbffbc39c4cd
pheonix8 <61965711+pheonix8@users.noreply.github.com>
2022-12-19 09:47:58 +0100

    updated analytics.py
     - implemented get_analytics function

--- analytics.py
+++ analytics.py
@@ -16,3 +16,39 @@ def find_dogs_by_name(name: str, year: str = ""):
                     int(dog["SexHundSort"])
                 ))
     return filtered_dogs
+
+
+def get_analytics(year: str = ""):
+    all_dogs = data.get_dog_data(year)
+
+    # Get the longest / shortest names
+    max_length = max([len(row["HundenameText"]) for row in all_dogs])
+    min_length = min([len(row["HundenameText"]) for row in all_dogs if row["HundenameText"] != "?"])
+
+    # stats
+    all_names_collection = collections.Counter([row["HundenameText"] for row in all_dogs])
+    m_names_collection = collections.Counter([row["HundenameText"] for row in all_dogs if row["SexHundSort"] == "1"])
+    f_names_collection = collections.Counter([row["HundenameText"] for row in all_dogs if row["SexHundSort"] == "2"])
+
+    analytics = {
+        "longestNames": list(
+            row["HundenameText"] for row in all_dogs if len(row["HundenameText"]) == max_length
+        ),
+        "shortestNames": list(
+            row["HundenameText"] for row in all_dogs if len(row["HundenameText"]) == min_length
+        ),
+        "topTenCommonNames": {
+            "all_names": all_names_collection.most_common(10),
+            "all_names_m": m_names_collection.most_common(10),
+            "all_names_f": f_names_collection.most_common(10),
+        },
+        "countOfDogs": {
+            "all_count": len([row for row in all_dogs]),
+            "m_count": len([row for row in all_dogs if row["SexHundSort"] == "1"]),
+            "f_count": len([row for row in all_dogs if row["SexHundSort"] == "2"]),
+
+        }
+
+    }
+
+    return analytics
